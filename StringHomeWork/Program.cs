719580edd615abf8472444f94619463b35e690e6
RiKo030 <riko03030303@gmail.com>
2019-06-09 16:53:44 +0300

    added 3 task

--- StringHomeWork/Program.cs
+++ StringHomeWork/Program.cs
@@ -11,10 +11,46 @@ namespace StringHomeWork
         static void Main(string[] args)
         {
             //---------------------1-------------------
+            Console.WriteLine("----------1----------");
             string sometext="Nikolay have one good friend, he's name Nikolay";
             Console.WriteLine(sometext);
             string newtext=sometext.Replace("Nikolay", "Oleg");
             Console.WriteLine(newtext);
+            Console.WriteLine();
+
+            //---------------------2----------------------
+            Console.WriteLine("----------2----------");
+            string str = "((8+2)*30)";
+            int left = 0,right=0;
+            Console.WriteLine(str);
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '(')
+                {
+                    left++;
+                }
+                if (str[i] == ')')
+                {
+                    right++;
+                }
+            }
+            if (left != right)
+            {
+                Console.WriteLine("Not equal");
+            }
+            else
+            {
+                Console.WriteLine("Equal");
+            }
+            Console.WriteLine();
+
+            //-----------------------3-----------------
+            Console.WriteLine("----------3----------");
+            string str1 = "Hello my dear friend";
+            string str2 = "my cat is a  good litle boy";
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            
         }
     }
 }
